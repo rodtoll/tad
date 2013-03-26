518f2dcf3e5ea16b896af986b578845211a84151
Rod Toll <rod_toll@hotmail.com>
2013-03-25 21:44:08 -0700

    Original

---
+++ tadmaster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tadmaster::Application.config.secret_token = '8bb488cba944294009d61ff4efa6a0515e02aa9c0dd30045d0d03258011ec576c928b7089cb2c2f4f496fb413b43e9b48490e28802ddca80d4fbeabb3031dbe5'
---
+++ tadmaster/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tadmaster::Application.config.session_store :cookie_store, key: '_tadmaster_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tadmaster::Application.config.session_store :active_record_store
